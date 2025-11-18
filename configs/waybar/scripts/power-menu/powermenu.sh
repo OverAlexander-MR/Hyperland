5864cb927ff69abf7da048b3b840e7592303ab61
OverAlexander-MR <omejiar@unal.edu.co>
2025-11-17 19:13:51 -0500

    power

--- configs/waybar/scripts/power-menu/powermenu.sh
+++ configs/waybar/scripts/power-menu/powermenu.sh
@@ -57,24 +57,21 @@ run_cmd() {
     elif [[ $1 == '--reboot' ]]; then
       systemctl reboot
     elif [[ $1 == '--suspend' ]]; then
-      mpc -q pause
-      amixer set Master mute
       systemctl suspend
-      swaylock-fancy
 
     elif [[ $1 == '--logout' ]]; then
       hyprctl dispatch exit 1
-      #if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-      #  openbox --exit
-      #elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-      #  bspc quit
-      #elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-      #  i3-msg exit
-      #elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-      #  qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-      #elif [[ "$DESKTOP_SESSION" == 'Hyprland' ]]; then
-      #  hyprctl dispatch exit 1
-      #fi
+      if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+       openbox --exit
+      elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+       bspc quit
+      elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+       i3-msg exit
+      elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+       qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+      elif [[ "$DESKTOP_SESSION" == 'Hyprland' ]]; then
+       hyprctl dispatch exit 1
+      fi
     fi
   else
     exit 0
